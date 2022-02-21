057fed928500ad4f4fe0aa35be46920514eb1816
Ben Walker <ben.walker@thinktribe.com>
2022-02-21 10:57:49 +0000

    First working version

--- inject.js
+++ inject.js
@@ -35,3 +35,17 @@ s.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
+
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('hover.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
+
+var link = document.createElement('link');
+link.rel  = 'stylesheet';
+link.type = 'text/css';
+link.href = chrome.runtime.getURL('hover.css');
+console.log(link);
+(document.head || document.documentElement).appendChild(link);
